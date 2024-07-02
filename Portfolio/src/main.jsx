5ca4eb8e4cb6b3634da2d7ff6a5b7653f385839f
gulshan <gk0507818@gmail.com>
2024-07-03 00:06:32 +0530

    added-app

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, HashRouter } from 'react-router-dom'
 import Layout from './Components/Layout.jsx'
 import About from './Components/About.jsx'
 import Home from './Components/Home.jsx'
@@ -12,7 +12,7 @@ import Project from './Components/Project.jsx'
 import Proj_htlh from './Components/Proj_htlh.jsx'
 import Project_bank from './Components/Project_bank.jsx'
 import Blogs from './Components/Blogs.jsx'
-const router=createBrowserRouter([
+const router=createHashRouter([
   {
     path:'/',
     element:<Layout/>,
@@ -53,9 +53,9 @@ const router=createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="portfolio_react">
+  <HashRouter basename="portfolio_react">
   <App />
-</BrowserRouter>,
+</HashRouter>,
   <React.StrictMode>
  <RouterProvider router={router}/>
   </React.StrictMode>
