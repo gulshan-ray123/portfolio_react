db4dcd2d07c566530c5b0885897a56f969be2141
gulshan <gk0507818@gmail.com>
2024-07-02 20:41:19 +0530

    added-app

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, HashRouter } from 'react-router-dom'
 import Layout from './Components/Layout.jsx'
 import About from './Components/About.jsx'
 import Home from './Components/Home.jsx'
@@ -12,7 +12,7 @@ import Project from './Components/Project.jsx'
 import Proj_htlh from './Components/Proj_htlh.jsx'
 import Project_bank from './Components/Project_bank.jsx'
 import Blogs from './Components/Blogs.jsx'
-const router=createBrowserRouter([
+const router=HashRouter([
   {
     path:'/',
     element:<Layout/>,
